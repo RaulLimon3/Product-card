c988ae25ea9ec0c8d52ab6bd6f6f482372dfeaf2
Raul <raul_l1m0n@hotmail.com>
2026-09-10 12:41:09 -0600

    feat: render dynamic product information

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Accedemos a nuestro endpoint
 const getSingleProduct = async () => {
     const SINGLE_PRODUCT_API = 'https://fakestoreapi.com/products/1';
     try {
@@ -7,7 +6,7 @@ const getSingleProduct = async () => {
             throw new Error(`Error HTTP: ${response.status}`);
         }
         const data = await response.json();
-        console.log(data)
+        handleProduct(data);
     } catch (error) {
         console.error(error);
     }
@@ -15,3 +14,33 @@ const getSingleProduct = async () => {
 
 getSingleProduct();
 
+const handleProduct = ({id, image, title, description, category, rating, price}) => {
+    const product = {
+        id,
+        image,
+        title,
+        description,
+        category,
+        rating,
+        price
+    };
+    renderProduct(product);
+};
+
+const imageProduct = document.getElementById('image');
+const titleProduct = document.getElementById('title');
+const descriptionProduct = document.getElementById('description');
+const categoryProduct = document.getElementById('category');
+const rateProduct = document.getElementById('rate');
+const countProduct = document.getElementById('count');
+const priceProduct = document.getElementById('price');
+
+const renderProduct = ({image, title, description, category, rating, price}) => {
+    imageProduct.src = image;
+    titleProduct.innerText = title;
+    descriptionProduct.innerText = description;
+    categoryProduct.innerText = category;
+    rateProduct.innerText = rating.rate;
+    countProduct.innerText = `(${rating.count})`;
+    priceProduct.innerText = `$${price}`;
+};
