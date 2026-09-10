b5282fae26e9980584684493ac4c9428e64c34c4
Raul <raul_l1m0n@hotmail.com>
2026-09-10 16:25:32 -0600

    feat: add HTTP error handling for product requests

--- js/main.js
+++ js/main.js
@@ -3,12 +3,29 @@ const getSingleProduct = async () => {
     try {
         const response = await fetch(SINGLE_PRODUCT_API);
         if (!response.ok) {
+            handleHttpError(response.status);
             throw new Error(`Error HTTP: ${response.status}`);
         }
         const data = await response.json();
         handleProduct(data);
     } catch (error) {
         console.error(error);
+    }
+};
+
+const handleHttpError = (status) => {
+    switch(status) {
+        case 400:
+            console.log('La solicitud no es válida');
+            break;
+        case 404:
+            console.log('El producto no fue encontrado, inténtalo mas tarde');
+            break;
+        case 500:
+            console.log('Ocurrió un error en el servidor, inténtalo mas tarde por favor');
+            break;
+        default:
+            console.log('No se pudo establecer una conexión estable, inténtalo mas tarde');
     }
 };
 
